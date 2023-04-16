9b1bd8e93961f7daf493a06b7ab6fe72f01c1e3a
Doyu-Lee <doyu.lee.0406@gmail.com>
2023-04-16 16:02:22 +0900

    feat: 서버(Firebase) 연결

---
+++ firebaseConfig.js
@@ -0,0 +1,29 @@
+import firebase from "firebase/compat/app";
+
+// 사용할 파이어베이스 서비스 주석을 해제합니다
+//import "firebase/compat/auth";
+import "firebase/compat/database";
+//import "firebase/compat/firestore";
+//import "firebase/compat/functions";
+import "firebase/compat/storage";
+
+// Initialize Firebase
+//파이어베이스 사이트에서 봤던 연결정보를 여기에 가져옵니다
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_API_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
+};
+
+//사용 방법입니다. 
+//파이어베이스 연결에 혹시 오류가 있을 경우를 대비한 코드로 알아두면 됩니다.
+if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+}
+
+export const firebase_db = firebase.database()
